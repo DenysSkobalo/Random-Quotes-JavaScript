055b6db298036c78f3d93a7a33d6978a47fc43e2
Denys <96637006+Micotoyami@users.noreply.github.com>
2022-06-03 18:48:53 +0300

    Add app.js

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-// const quoteText = document.querySelector('.quote')
-// const quoteBtn =document.querySelector('button');
+const btn = document.getElementById('btn');
+const output_quote = document.getElementById('output');
 
-// const quote__aray = [
-//     {
-//         text: 'I think it is fair to say that personal computers have become the most empowering tool we have ever created. They are tools of communication, they are tools of creativity, and they can be shaped by their user.',
-//         author: 'Bill Gates' 
-//     },
-//     {
-//         text: 'Everybody should learn to program a computer, because it teaches you how to think.',
-//         author: 'Steve Jobs' 
-//     },
-//     {
-//         text: 'I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12.',
-//         author: 'Elon Musk'
-//     }
+// Array with quotes
+// Масив з цитатами
+const quotes = 
+    [
+        'I think it is fair to say that personal computers have become the most empowering tool we have ever created. They are tools of communication, they are tools of creativity, and they can be shaped by their user. <br> -Bill Gates',
+        'Everybody should learn to program a computer, because it teaches you how to think. <br> - Steve Jobs',
+        'I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12.<br> - Elon Musk',
+    ];
 
-// ]
+// Add an event to the button
+// Додаємо подію кнопці
+btn.addEventListener('click', randomQuotes)
 
-// quoteBtn.addEventListener('click', randomQuote);
-
-// function randomQuote() {
-        
-// }
+// Function
+// Функція
+function randomQuotes() {
+    let random =  quotes[Math.floor(Math.random() * quotes.length)];
+    output_quote.innerHTML = random;
+}
