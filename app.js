9c9923c4191561d10ba4387e4805f52683ed1e19
Denys <96637006+Micotoyami@users.noreply.github.com>
2022-06-03 18:15:42 +0300

    Redisgn project

---
+++ app.js
@@ -0,0 +1,24 @@
+// const quoteText = document.querySelector('.quote')
+// const quoteBtn =document.querySelector('button');
+
+// const quote__aray = [
+//     {
+//         text: 'I think it is fair to say that personal computers have become the most empowering tool we have ever created. They are tools of communication, they are tools of creativity, and they can be shaped by their user.',
+//         author: 'Bill Gates' 
+//     },
+//     {
+//         text: 'Everybody should learn to program a computer, because it teaches you how to think.',
+//         author: 'Steve Jobs' 
+//     },
+//     {
+//         text: 'I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12.',
+//         author: 'Elon Musk'
+//     }
+
+// ]
+
+// quoteBtn.addEventListener('click', randomQuote);
+
+// function randomQuote() {
+        
+// }
